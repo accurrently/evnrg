9784abc0afa41e524fcbcb3ab6c1195fa1d5b81a
Alex Campbell <alex@acamp.me>
2019-04-29 20:13:58 -0700

    Updated requirements

--- requirements.py
+++ requirements.py
@@ -3,10 +3,9 @@ requirements = [
     'numpy>=1.15.0',
     'pandas>=0.23.0',
     'appdirs',
-    'apache-libcloud'
+    'apache-libcloud',
+    'pycrypto'
 ]
 
-dependency_links = [
-    'https://github.com/SeguinBe/dask_k8/tarball/master'
-]
+dependency_links = []
 
