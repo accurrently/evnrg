eed56c47a94fd4b2b6d160716856fcc50cff4113
Alex Campbell <alex@acamp.me>
2019-04-30 09:22:02 -0700

    Update requirements

--- requirements.py
+++ requirements.py
@@ -1,7 +1,10 @@
 requirements = [
     'numba>=0.42.0',
-    'numpy>=1.15.0',
-    'pandas>=0.23.0',
+    'numpy>=1.16.0',
+    'pandas>=0.24.0',
+    'dask>=1.1.2',
+    'distributed>=1.27.0',
+    'fastparquet>=0.2.1',
     'appdirs',
     'apache-libcloud',
     'pycrypto'
