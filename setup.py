402e00f2c1f2ca2345d7b6172f3e4681ac8a15ab
Alex Campbell <alex@acamp.me>
2019-04-29 18:47:57 -0700

    v 0.1.4

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/accurrently/evnrg',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
     dependency_links=dependency_links
 )
