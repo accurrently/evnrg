4ade036637bb2f0285e8b10ddfcbee223d640576
Alex Campbell <alex@acamp.me>
2019-04-30 10:28:25 -0700

    Removed requirements because of bugs in psutil and fastparquet wheel building via pip

--- requirements.py
+++ requirements.py
@@ -1,11 +1,11 @@
 requirements = [
-    'numba>=0.42.0',
-    'numpy>=1.16.0',
-    'pandas>=0.24.0',
-    'dask>=1.1.2',
-    'distributed>=1.27.0',
-    'appdirs',
-    'apache-libcloud'
+    #'numba>=0.42.0',
+    #'numpy>=1.16.0',
+    #'pandas>=0.24.0',
+    #'dask',
+    #'distributed',
+    #'appdirs',
+    #'apache-libcloud'
 ]
 
 dependency_links = []
