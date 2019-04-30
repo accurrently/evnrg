a178d3513d0d02366f6e9583d661fc5b85059691
Alex Campbell <alex@acamp.me>
2019-04-29 19:38:23 -0700

    MOved to v 0.1.5 by flattening module

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/accurrently/evnrg',
-    version='0.1.4-1',
+    version='0.1.5',
     zip_safe=False,
     dependency_links=dependency_links
 )
