b2acafe8d83e9ea63926c270e04eed32a01e5557
Alex Campbell <alex@acamp.me>
2019-05-02 15:01:27 -0700

    changed name from numbaloop

--- evnrg/daskjob.py
+++ evnrg/daskjob.py
@@ -13,7 +13,7 @@ from .dataset import DatasetInfo
 from .datastorage import StorageInfo
 #from .job_results import JobResults
 #from .simulation import run_simulation, SimulationResult
-from .numbaloop import run_simulation as nb_run_simulation
+from .simulation_nb import run_simulation as nb_run_simulation
 
 
 class DaskJobRunner(object):
