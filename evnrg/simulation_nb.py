0da01714d31512a7b886b81fdd4f2280e4851ee3
Alex Campbell <alex@acamp.me>
2019-05-02 17:23:27 -0700

    remove typo

--- evnrg/simulation_nb.py
+++ evnrg/simulation_nb.py
@@ -69,7 +69,7 @@ def make_evse_banks(evse_banks: list, fleet_size: int):
         bank_running_power = 0
         n_in_bank = 0
         e: EVSEType
-        for e in bank['evse']):
+        for e in bank['evse']:
             copies = round(fleet_size * e.pro_)
             if e.max_ > 0:
                 copies = int(min(e.max_, copies))
